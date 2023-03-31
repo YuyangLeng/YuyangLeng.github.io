a9d4e7b47c0177eb95f6ccadbd95abd44782aa06
HaoLiang1017 <55616343+YuyangLeng@users.noreply.github.com>
2023-03-30 21:23:34 -0400

    4

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let sectionList = document.querySelectorAll('section');
+document.addEventListener("DOMContentLoaded", function () {
+  let sectionList = document.querySelectorAll("section");
 
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    return rect.top <= windowHeight && rect.bottom >= 0;
+  function fadeIn(item, index) {
+    setTimeout(function () {
+      item.style.opacity = "1";
+    }, 150 * index);
   }
 
-  function fadeInOnScroll() {
-    sectionList.forEach(function (section) {
-      let items = section.querySelectorAll('li');
-      items.forEach(function (item, index) {
-        if (isInViewport(item)) {
-          setTimeout(function () {
-            item.style.opacity = '1';
-          }, 150 * index);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const items = entry.target.querySelectorAll("li");
+          items.forEach(fadeIn);
+          observer.unobserve(entry.target);
         }
       });
-    });
-  }
+    },
+    {
+      threshold: 0.1,
+    }
+  );
 
-  window.addEventListener('scroll', fadeInOnScroll);
-  fadeInOnScroll();
+  sectionList.forEach((section) => {
+    observer.observe(section);
+  });
 });
