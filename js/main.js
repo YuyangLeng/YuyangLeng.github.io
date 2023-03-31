d38331fedbfc197bd8a0620cc9b35fb4ee9c3351
HaoLiang1017 <55616343+YuyangLeng@users.noreply.github.com>
2023-03-30 21:17:27 -0400

    3

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ document.addEventListener('DOMContentLoaded', function() {
     
     window.addEventListener('scroll', fadeInOnScroll);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let sectionList = document.querySelectorAll('section');
+
+    function fadeInOnScroll() {
+        let scrollPos = window.scrollY + window.innerHeight;
+
+        sectionList.forEach(function(section) {
+            let items = section.querySelectorAll('li');
+            items.forEach(function(item, index) {
+                if (scrollPos > item.offsetTop + section.offsetTop) {
+                    setTimeout(function() {
+                        item.style.opacity = '1';
+                    }, 150 * index);
+                }
+            });
+        });
+    }
+
+    window.addEventListener('scroll', fadeInOnScroll);
+});
