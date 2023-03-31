94b8ec225892677d6070f0a3ac6637f60e665cec
HaoLiang1017 <55616343+YuyangLeng@users.noreply.github.com>
2023-03-30 21:21:40 -0400

    4

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let sectionList = document.querySelectorAll("section");
+  let sectionList = document.querySelectorAll('section');
 
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    const html = document.documentElement;
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || html.clientHeight) &&
-      rect.right <= (window.innerWidth || html.clientWidth)
-    );
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight && rect.bottom >= 0;
   }
 
   function fadeInOnScroll() {
     sectionList.forEach(function (section) {
-      let items = section.querySelectorAll("li");
+      let items = section.querySelectorAll('li');
       items.forEach(function (item, index) {
         if (isInViewport(item)) {
           setTimeout(function () {
-            item.style.opacity = "1";
+            item.style.opacity = '1';
           }, 150 * index);
         }
       });
     });
   }
 
-  window.addEventListener("scroll", fadeInOnScroll);
+  window.addEventListener('scroll', fadeInOnScroll);
   fadeInOnScroll();
 });
